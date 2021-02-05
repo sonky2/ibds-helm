0809ca04f7528a49ff654eeb8ef6c4d1b382113e
宋明明 <songmingming@inspur.com>
2021-02-05 14:40:55 +0800

    modifyValues_3.sh脚本实现自动化替换caFileName

---
+++ modifyValues_3.sh
@@ -0,0 +1,34 @@
+#!/bin/bash +x
+#
+# Copyright IBM Corp. All Rights Reserved.
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+#set -e
+
+CURRENT_DIR=$PWD;
+destDir=./helm;
+if [ ! -d "$destDir" ]; then
+  mkdir -p "$destDir"
+fi
+echo "当前执行名录是："${CURRENT_DIR};
+
+## Create Peer Config Files
+
+cp  ${destDir}/values-template.yaml  ${destDir}/values.yaml;
+for k in $(seq 1 $1)
+do
+  ORG_NO=org${k};
+  ORG_B_NO=Org${k};
+
+  echo "##########################################################"
+  echo "##### replace ${ORG_NO}.example.com  *_sk  #########"
+  echo "##########################################################"
+  cd  ./crypto-config/peerOrganizations/${ORG_NO}-example-com/ca/;
+  PRIV_KEY=$(ls *_sk)
+
+  cd $CURRENT_DIR;
+  sed -i "s/CA_FILE_NAME${k}/${PRIV_KEY}/g" ${destDir}/values.yaml;
+  
+done;
